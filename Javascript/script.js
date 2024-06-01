2399310f9f4ce74d1de6c3390de828cc892495c3
minju9866 <78875000+minju9866@users.noreply.github.com>
2024-06-01 19:04:57 +0900

    Summary

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,3 +3,7 @@ import './login.js';
 import './mainpage-mypage.js';
 import './logout.js';
 import './lecture-upload.js';
+
+document.querySelector('.newlecture-upload').addEventListener('click', function() {
+    window.location.href = 'lecture-upload.html';
+  });
