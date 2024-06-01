fc90d2a8347d46c20d3a29d5a1fdbb94c6ccbbf4
minju9866 <78875000+minju9866@users.noreply.github.com>
2024-06-02 02:49:39 +0900

    Summary

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,7 +5,6 @@ import './logout.js';
 import './lecture-upload.js';
 import './search-input.js';
 import './review-board.js';
-import './question-board.js';
 import './category-lecture.js';
 import './favorite-lecture.js';
 import './lecture-tutor.js';
